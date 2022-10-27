481cb25526e84f8b7d70ed799d0af1e0106f40c3
00012938 <abdulazizdusbabaev@gmail.com>
2022-10-27 21:20:24 +0500

    input from user is added to the story and it is seen on console

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 //adding original text
-let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`
+let originalStory = 'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}'
 
-userInputs = [] //array for storing the inputs
+let userInputs = [] //array for storing the inputs
+numberOfQuestions = 3; //total number of promps that will appear
+questions = ["Enter a verb ", "Enter a noun ", "Enter an adjective "]
+for (question of questions){
+    let input = prompt(question)
+    userInputs.push(input)
+}
+let StoryWithInput = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`
+console.log(userInputs)
+console.log(StoryWithInput)
