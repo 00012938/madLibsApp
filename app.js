ad329afc97dbdab2268f855a43438533cba60b13
00012938 <abdulazizdusbabaev@gmail.com>
2022-10-27 20:45:02 +0500

    init: row variables

---
+++ app.js
@@ -0,0 +1,4 @@
+//adding original text
+let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`
+
+userInputs = [] //array for storing the inputs
